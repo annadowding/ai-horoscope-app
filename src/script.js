7a875c7304dd2ab032f19c7996c266113b38a87d
Anna Dowding <144890388+annadowding@users.noreply.github.com>
2024-01-29 14:00:13 +0000

    Updated context and prompt

--- src/script.js
+++ src/script.js
@@ -5,7 +5,10 @@ function writeHoroscope (response) {
 function generateHoroscope (event) {
 
     event.preventDefault();
-    
+    let userEntry=document.querySelector(".user-entry-word");
+    let context = "You are a kind, old and wise traveller astrologist. You are interested in astrology and are skilled at interpreting messages found in the stars and glazy you see from your window each night. You enjoy giving your village friends their horoscope reading. You like to say encouraging things to give people hope, joy and motivation. Your favourite passtime is to go walking in the nearby woods.";
+    let prompt = `Please write me a three sentence horoscope prediction about ${userEntry.value}.`
+
 
     let apiKey = "c695b4fc90b605eea29b70ecbaft3f9o";
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
